feb88b2efb85e5242c1c667734cb64ce0a73debe
Otto Cedeno <otto@ottostacos.com>
2019-11-05 23:19:27 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed.between?(40, 60)
+    return false
+  else
+    return true
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  return speed.between?(40, 60) ? false : true
 end
-	
-
-
